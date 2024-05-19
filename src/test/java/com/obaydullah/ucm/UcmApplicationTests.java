3f1b693cfb05d7ad5eb31db40bfedfebec6c104d
Obaydullah <obaydullahkhaan@gmail.com>
2024-05-19 19:21:44 +0600

    Initial commit

---
+++ src/test/java/com/obaydullah/ucm/UcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obaydullah.ucm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
